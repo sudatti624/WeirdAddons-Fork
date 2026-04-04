40325dfd6ea57f0c4d7c79d212a0059d6bb1f0ac
sudatti624 <165920415+sudatti624@users.noreply.github.com>
2026-04-04 23:17:49 +0900

    Refactor mixins for improved functionality and code clarity

--- src/main/java/weirdaddons/WeirdAddonsCommands.java
+++ src/main/java/weirdaddons/WeirdAddonsCommands.java
@@ -3,6 +3,7 @@ package weirdaddons;
 import carpet.settings.SettingsManager;
 import com.mojang.brigadier.CommandDispatcher;
 import net.minecraft.command.argument.ColumnPosArgumentType;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.util.math.ChunkPos;
 import net.minecraft.util.math.ColumnPos;
@@ -16,18 +17,19 @@ import static net.minecraft.server.command.CommandManager.argument;
 
 public class WeirdAddonsCommands {
 
-    public static void register(CommandDispatcher<ServerCommandSource> dispatcher) { // This is totally not sketch or anything. Totally didn't just throw this together within a couple minutes...
+    public static void register(CommandDispatcher<ServerCommandSource> dispatcher) {
         dispatcher.register(literal("weird").requires((player) -> SettingsManager.canUseCommand(player, WeirdAddonsSettings.commandWeird)).
                 then(literal("chunk").
                         then(literal("watch").
                                 executes((c) -> {
+                                    MinecraftServer server = c.getSource().getServer();
                                     UUID playerUUID = c.getSource().getPlayer().getUuid();
                                     if (WeirdAddonsUtils.playersWatching.contains(playerUUID)) {
                                         WeirdAddonsUtils.playersWatching.remove(playerUUID);
-                                        WeirdAddonsUtils.sendToPlayer(playerUUID, "You are no longer watching chunk activity!");
+                                        WeirdAddonsUtils.sendToPlayer(server, playerUUID, "You are no longer watching chunk activity!");
                                     } else {
                                         WeirdAddonsUtils.playersWatching.add(playerUUID);
-                                        WeirdAddonsUtils.sendToPlayer(playerUUID, "You are now watching chunk activity!");
+                                        WeirdAddonsUtils.sendToPlayer(server, playerUUID, "You are now watching chunk activity!");
                                     }
                                     return 1;
                                 })
@@ -37,31 +39,35 @@ public class WeirdAddonsCommands {
                                         executes((c) -> {
                                             ColumnPos columnPos = ColumnPosArgumentType.getColumnPos(c, "chunk");
                                             WeirdAddonsSettings.chunkPos = new ChunkPos(columnPos.x, columnPos.z);
-                                            WeirdAddonsUtils.sendToPlayer(c.getSource().getPlayer().getUuid(), "Targeted chunk set to: " + WeirdAddonsSettings.chunkPos);
+                                            WeirdAddonsUtils.sendToPlayer(c.getSource().getServer(), c.getSource().getPlayer().getUuid(), "Targeted chunk set to: " + WeirdAddonsSettings.chunkPos);
                                             return 1;
                                         }))).
                         then(literal("start").
                                 executes((c) -> {
+                                    MinecraftServer server = c.getSource().getServer();
+                                    UUID playerUUID = c.getSource().getPlayer().getUuid();
                                     if (WeirdAddonsSettings.chunkPos == null) {
-                                        WeirdAddonsUtils.sendToPlayer(c.getSource().getPlayer().getUuid(), "A chunk must be specified before this can be enabled!");
+                                        WeirdAddonsUtils.sendToPlayer(server, playerUUID, "A chunk must be specified before this can be enabled!");
                                     } else if (WeirdAddonsSettings.isDisplayingChunk) {
-                                        WeirdAddonsUtils.sendToPlayer(c.getSource().getPlayer().getUuid(), "Chunk has already started!");
+                                        WeirdAddonsUtils.sendToPlayer(server, playerUUID, "Chunk has already started!");
                                     } else {
-                                        WeirdAddonsUtils.playersWatching.add(c.getSource().getPlayer().getUuid());
+                                        WeirdAddonsUtils.playersWatching.add(playerUUID);
                                         WeirdAddonsSettings.chunkWorld = c.getSource().getPlayer().world;
                                         WeirdAddonsSettings.isDisplayingChunk = true;
-                                        WeirdAddonsUtils.sendToPlayer(c.getSource().getPlayer().getUuid(), "Chunk has started!");
+                                        WeirdAddonsUtils.sendToPlayer(server, playerUUID, "Chunk has started!");
                                     }
                                     return 1;
                                 })
                         ).
                         then(literal("stop").
                                 executes((c) -> {
+                                    MinecraftServer server = c.getSource().getServer();
+                                    UUID playerUUID = c.getSource().getPlayer().getUuid();
                                     if (WeirdAddonsSettings.isDisplayingChunk) {
                                         WeirdAddonsSettings.isDisplayingChunk = false;
-                                        WeirdAddonsUtils.sendToPlayer(c.getSource().getPlayer().getUuid(), "Chunk has stopped!");
+                                        WeirdAddonsUtils.sendToPlayer(server, playerUUID, "Chunk has stopped!");
                                     } else {
-                                        WeirdAddonsUtils.sendToPlayer(c.getSource().getPlayer().getUuid(), "Chunk is not running!");
+                                        WeirdAddonsUtils.sendToPlayer(server, playerUUID, "Chunk is not running!");
                                     }
                                     WeirdAddonsUtils.playersWatching.clear();
                                     return 1;
@@ -71,7 +77,7 @@ public class WeirdAddonsCommands {
                                 then(argument("radius", integer(0)).
                                         executes((c) -> {
                                             WeirdAddonsSettings.chunkRadius = getInteger(c, "radius");
-                                            WeirdAddonsUtils.sendToPlayer(c.getSource().getPlayer().getUuid(), "Chunk Radius has been set to: " + WeirdAddonsSettings.chunkRadius);
+                                            WeirdAddonsUtils.sendToPlayer(c.getSource().getServer(), c.getSource().getPlayer().getUuid(), "Chunk Radius has been set to: " + WeirdAddonsSettings.chunkRadius);
                                             return 1;
                                         }))
                         )
@@ -83,37 +89,39 @@ public class WeirdAddonsCommands {
                                         executes((c) -> setChunkTps(c.getSource(), getFloat(c, "rate")))))).*/
                         then(literal("control").
                         executes((c) -> {
-                            WeirdAddonsUtils.sendToPlayer(c.getSource().getPlayer().getUuid(), "Not implemented yet...");
+                            WeirdAddonsUtils.sendToPlayer(c.getSource().getServer(), c.getSource().getPlayer().getUuid(), "Not implemented yet...");
                             return 1;
                         })
                 ).
                         //Just ignore this
                         then(literal("color").
                                 executes((c) -> {
-                                    WeirdAddonsUtils.sendToPlayer(c.getSource().getPlayer().getUuid(),
-                                            "§l----------: §6§lColors: §r§l :----------" + "\n"
-                                                    + "§aGreen: &a" + "\n"
-                                                    + "§bAqua: &b" + "\n"
-                                                    + "§cRed: &c" + "\n"
-                                                    + "§dPink: &d" + "\n"
-                                                    + "§eYellow: &e" + "\n"
-                                                    + "§fWhite: &f" + "\n"
-                                                    + "§0Black: &0" + "\n"
-                                                    + "§1Dark Blue: &1" + "\n"
-                                                    + "§2Dark Green: &2" + "\n"
-                                                    + "§3Dark Aqua: &3" + "\n"
-                                                    + "§4Dark Red: &4" + "\n"
-                                                    + "§5Dark Purple: &5" + "\n"
-                                                    + "§6Gold: &6" + "\n"
-                                                    + "§7Grey: &7" + "\n"
-                                                    + "§8Dark Grey &8" + "\n"
-                                                    + "§9Indigo: &9§r" + "\n"
-                                                    +"§l----------: §6§lFormats: §r§l :----------§r" + "\n"
-                                                    + "Obfuscate: &k" + "\n"
-                                                    + "§lBold: &l" + "\n"
-                                                    + "§mStrike Through: &m§r" + "\n"
-                                                    + "§nUnderline: &n§r" + "\n"
-                                                    + "§oItalics &o§r" + "\n");
+                                    WeirdAddonsUtils.sendToPlayer(c.getSource().getServer(), c.getSource().getPlayer().getUuid(),
+                                            """
+                                            §l----------: §6§lColors: §r§l :----------
+                                            §aGreen: &a
+                                            §bAqua: &b
+                                            §cRed: &c
+                                            §dPink: &d
+                                            §eYellow: &e
+                                            §fWhite: &f
+                                            §0Black: &0
+                                            §1Dark Blue: &1
+                                            §2Dark Green: &2
+                                            §3Dark Aqua: &3
+                                            §4Dark Red: &4
+                                            §5Dark Purple: &5
+                                            §6Gold: &6
+                                            §7Grey: &7
+                                            §8Dark Grey &8
+                                            §9Indigo: &9§r
+                                            §l----------: §6§lFormats: §r§l :----------§r
+                                            Obfuscate: &k
+                                            §lBold: &l
+                                            §mStrike Through: &m§r
+                                            §nUnderline: &n§r
+                                            §oItalics &o§r
+                                            """);
                                     return 1;
                                 })
                         ));
